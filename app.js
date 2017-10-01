42f53837da7fe8d009ba3346e3bc4046b0b9b16c
James <s3562715@student.rmit.edu.au>
2017-10-01 05:04:38 +0000

    added database - persisting data

--- app.js
+++ app.js
@@ -1,28 +1,61 @@
-var express = require("express");
-var app = express();
+var express     = require("express");
+var app         = express();
+var bodyParser  = require("body-parser");
+var mongoose    = require("mongoose");
+
+mongoose.connect("mongodb://localhost/campolo", {useMongoClient: true});
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
+
+// INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res){
-   var campgrounds = [
-            {
-                name: "Byron Bay", image: "https://farm7.staticflickr.com/6186/6090714876_44d269ed7e.jpg"
-            },
-            {
-                name: "Tamborine Mountain", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg"
-            },
-            {
-                name: "Sunshine Coast", image: "https://farm3.staticflickr.com/2535/3823437635_c712decf64.jpg"
-            }
-       ];
-       
-       res.render("campgrounds", {campgrounds: campgrounds});
-    
+    // get all camgpgrounds from db
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    });
 });
+
+// CREATE - add new campgrounds to database
+app.post("/campgrounds", function(req, res){
+   //get data from form and add to campgrounds array
+   var name = req.body.name;
+   var image = req.body.image;
+   var newCampground = {name: name, image: image};
+   
+   //create new campground and save to db
+   Campground.create(newCampground, function(err, newlyCreated){
+       if(err){
+           console.log(err);
+       } else {
+           res.redirect("/campgrounds");
+       }
+   });
+});
+
+
+// NEW - show form to create new campground
+app.get("/campgrounds/new", function(req, res){
+    res.render("new");
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Campolo Server Started!");
